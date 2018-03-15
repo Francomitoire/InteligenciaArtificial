967cad15c5447acd6bc2e011dfc1b00543140993
zomwwt <matias.iz71@gmail.com>
2018-03-15 14:23:21 -0300

    editor d texto

--- exampleinterface.py
+++ exampleinterface.py
@@ -1,8 +1,11 @@
 import wx
+class TestFrame(wx.Frame):
+  def __init__(self, parent, title):
+    wx.Frame.__init__(self, parent, title=title, size=(200,100))
+    self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
 if __name__ == '__main__':
   app = wx.App()
-  frame = wx.Frame(None, wx.ID_ANY, "Gatos")
+  frame = TestFrame(None, 'Editor de Texto')
+  frame.Center()
   frame.Show()
   app.MainLoop()
-
-#asdfg
