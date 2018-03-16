e2ac3864cb07eba3a01bddb617ed419d0ba8e431
zomwwt <37410215+zomwwt@users.noreply.github.com>
2018-03-15 21:04:12 -0300

    msje asd

--- exampleinterface.py
+++ exampleinterface.py
@@ -1,7 +1,7 @@
 import wx
 class TestFrame(wx.Frame):
   def __init__(self, parent, title):
-    wx.Frame.__init__(self, parent, title=title, size=(200,100))
+    wx.Frame.__init__(self, parent, title=title, size=(500,300))
     self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
 if __name__ == '__main__':
   app = wx.App()
